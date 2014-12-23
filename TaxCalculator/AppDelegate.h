614bea74f540033797bb64c023b36f8934a06c65
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2014-12-22 23:04:40 -0500

    Initial Commit

---
+++ TaxCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalculator
+//
+//  Created by Don Wettasinghe on 12/22/14.
+//  Copyright (c) 2014 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculator
+//
+//  Created by Don Wettasinghe on 12/22/14.
+//  Copyright (c) 2014 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
