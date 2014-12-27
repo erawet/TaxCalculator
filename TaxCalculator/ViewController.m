3da82b0f2f30f5c5b82bffccb45a671106b3a918
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2014-12-27 02:02:59 -0500

    Complet Project

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -10,18 +10,53 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UIButton *onCalculateButtonTapped;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
+
+- (IBAction)calTax:(UIButton *)sender;
+
+
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _caTax=7.5;
+    _chiTax=9.25;
+    _nyTax=4.5;
+    
 }
+
+- (IBAction)calTax:(UIButton *)sender {
+    
+    NSNumberFormatter *currencyFormatter=[[NSNumberFormatter alloc]init];
+    [currencyFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    
+    if (_segmentedControl.selectedSegmentIndex==0) {
+       
+        _resultLabel.text = [NSString stringWithFormat:@"%.02f" ,  _caTax * [_priceTextField.text doubleValue] ];
+        
+    } else if (_segmentedControl.selectedSegmentIndex==1){
+        _resultLabel.text = [NSString stringWithFormat:@"%.02f" ,  _chiTax * [_priceTextField.text doubleValue] ];
+        
+    } else {
+        _resultLabel.text = [NSString stringWithFormat:@"%.02f" ,  _nyTax * [_priceTextField.text doubleValue] ];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
